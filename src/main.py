af159533a79eafb009d15297bd8bd8f1b36f407c
mohamad <mobile.mohamad0789@gmail.com>
2025-03-30 20:37:13 +0300

    Update requirements and change server port in main.py. Added 'six' to requirements and changed Uvicorn server port from 8001 to 8002.

--- src/main.py
+++ src/main.py
@@ -43,4 +43,4 @@ async def extract_endpoint(request: RequestExtract):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
